d8bc303a0a42a1b2d8594b60ea26104d9ffa8ca7
shimizu <noon3m.op.sh@gmail.com>
2025-03-22 10:15:10 +0900

    [add] 使用する構造体

--- db/struct.go
+++ db/struct.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -8,9 +9,28 @@ type Database struct {
 	client *mongo.Client
 }
 
+type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	UserId   string             `bson:"user_id"`
+	Name     string             `bson:"username"`
+	Password string             `bson:"password"`
+	Email    string             `bson:"email"`
+}
+
+type JsonRequest struct {
+	UserId   string `json:"user_id"`
+	Password string `json:"password"`
+}
+
 type Comment struct {
-	ID        string `bson:"_id,omitempty"`
-	Name      string `bson:"name"`
-	Comment   string `bson:"comment"`
-	SessionId string `bson:"session_id"`
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Name      string             `bson:"name"`
+	Comment   string             `bson:"comment"`
+	SessionId string             `bson:"session_id"`
+}
+
+type PresentationUrl struct {
+	ID     primitive.ObjectID `bson:"_id,omitempty"`
+	UserId string             `bson:"user_id"`
+	Url    string             `bson:"url"`
 }
